e900ad37b22486b76392c1c2003feda445afa937
options3224@gmail.com <options3224@gmail.com>
2023-05-03 13:49:56 +0900

    config.py 파일 수정

--- CNN/config.py
+++ CNN/config.py
@@ -1,4 +1,8 @@
 import os
+import torch
+
+# 학습 횟수(에포크)
+epochs = 50
 
 # 현재 디렉토리
 dir_abs = os.path.dirname(os.path.abspath(__file__))    # 절대 경로
@@ -7,3 +11,6 @@ dir_rel = './' + os.path.basename(dir_abs)              # 상대 경로
 # 데이터셋 디렉토리
 dir_train = dir_abs + '/custom_dataset/train/'
 dir_test = dir_abs + '/custom_dataset/test/'
+
+# device 객체
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
